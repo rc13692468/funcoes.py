108ceb8642be858d34732299c6b5652e4ff7999d
rigoberto <rigobertosousa7@gmail.com>
2022-11-04 21:34:56 -0300

    adicionando  jogos

---
+++ revisao pra prova.py
@@ -0,0 +1,46 @@
+#dias = int(input("quantidade de dias "))
+#print(dias*24)
+
+#num = str(int(2**1000000))
+#print('A quantidade de digito e: %s' %len(num))
+
+#lista (10,11,20,22,30,33,40,44,50,55,60,66,70,77,80,88,90,99)
+#numeros_pares = (par)
+#nnumeros_inpares =(inpares)
+
+
+
+
+#import datetime
+#print('bom dia ')and (5 , 12)
+
+#populaçao = p 
+#amontada = a 
+#miraima = m 
+
+#a = 10000
+#m = 45000
+
+
+list1 = []
+list2 = []
+for  i in range(5):
+    l = input("digite 5 numeros:")
+    list1.append(l)
+    print(list1)
+
+    l2 = input("digite mais 5 :")
+    list2.append(l2)
+    
+    lista3 = (list1 + list2)
+    print(lista3)   
+
+
+
+
+
+
+
+
+
+
